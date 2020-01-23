00b88ac1cbab1d1a065444e21f706a528df2c858
Meireles <ext.paulo.meireles@farfetch.com>
2020-01-23 10:33:44 +0000

    onother files

---
+++ Programas/wifi2/wifi2/wifi2/wifi2.ino
@@ -0,0 +1,27 @@
+#include <SD.h>
+
+#include <WiFiEsp.h>
+#include <WiFiEspClient.h>
+#include <WiFiEspServer.h>
+#include <WiFiEspUdp.h>
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.begin("Casa_Santa_Teresinha", "teresinha");
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {}
